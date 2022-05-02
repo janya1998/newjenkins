a56e7d7fbcae5937492b446f72640885507e94ed
janya1998 <soujanyas2712@gmail.com>
2022-05-02 22:09:50 +0530

    this is my new commit

---
+++ src/test/java/AutoJenkins/CraeteContact.java
@@ -0,0 +1,23 @@
+package AutoJenkins;
+
+import org.testng.annotations.Test;
+
+public class CraeteContact {
+
+	public class CreateOrgTest {
+		@Test
+		public void orgTest1() {
+			System.out.println("Org Created");
+		}
+		@Test
+		public void orgTest() {
+			System.out.println("Org Created");
+		}
+		@Test
+		public void orgTest3() {
+			System.out.println("Org Created");
+		}
+	}
+}
+
+
