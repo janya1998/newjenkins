3c04841ad122753d957d486270736b69992672a0
janya1998 <soujanyas2712@gmail.com>
2022-05-02 23:19:19 +0530

    this is my commit

---
+++ src/test/java/AutoJenkins/CreateContactTest.java
@@ -0,0 +1,13 @@
+package AutoJenkins;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void orgTest() 
+	{
+		System.out.println("Org Created");
+	}
+
+}
